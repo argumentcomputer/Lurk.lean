5be7096663c76c7559214dd9a7d44059c173e8a0
mpenciak <matej.penciak@gmail.com>
2022-07-18 13:46:45 -0400

    lakefile basics and yatimastdlib dependency

--- lakefile.lean
+++ lakefile.lean
@@ -1,15 +1,12 @@
 import Lake
 open Lake DSL
 
-package lurk {
-  -- add package configuration options here
-}
-
-lean_lib Lurk {
-  -- add library configuration options here
-}
+package lurk 
 
 @[defaultTarget]
 lean_exe lurk {
   root := `Main
 }
+
+require YatimaStdLib from git
+  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "80b290a322267aee7dbca96b2547fa24de64236a"
