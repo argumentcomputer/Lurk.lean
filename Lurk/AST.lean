bc1e6f118b9ffa47fc5dc7caa87429728bf89354
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-10-05 12:31:39 -0300

    migrating code

--- Lurk/AST.lean
+++ Lurk/AST.lean
@@ -1,62 +1,101 @@
+import Lurk.SExpr
+import Lurk.PreUtils
+import Lurk.FixName
+
 namespace Lurk
 
-/-- Unary operations on Lurk expressions -/
-inductive UnaryOp | car | cdr | atom | emit
-deriving Repr, BEq
-
 /-- Binary operations on Lurk numerals -/
-inductive BinOp | cons | strcons | sum | diff | prod | quot | eq | nEq
+inductive BinaryOp | sum | diff | prod | quot | numEq | lt | gt | le | ge | eq
 deriving Repr, BEq
-
-/-- Basic Lurk primitives -/
-inductive Literal
-  -- `t` `nil`
-  | t | nil
-  -- Numerical values
-  | num     : Int → Literal
-  -- Strings
-  | str     : String → Literal
-  -- Characters
-  | char    : Char → Literal
-  -- Symbols
-  | sym     : String → Literal
-  deriving Repr
-
-inductive SExpr where
-  | atom : String → SExpr
-  | num  : Int → SExpr
-  | str  : String → SExpr
-  | char : Char → SExpr
-  | list : List SExpr → SExpr
-  | cons : SExpr → SExpr → SExpr
-  deriving Repr
 
 /-- Basic Lurk expression AST -/
 inductive Expr where
   -- `t`, `nil`, numeric, string and char literals
-  | lit   : Literal → Expr
+  | lit      : Literal → Expr
+  -- Symbols to reference content in the environment
+  | sym      : Name → Expr
   -- `if <test> <consequent> <alternate>`
-  | ifE     : Expr → Expr → Expr → Expr
+  | ifE      : Expr → Expr → Expr → Expr
   -- `lambda <formals> <body>`
-  | lam     : List String → Expr → Expr
+  | lam      : List Name → Expr → Expr
   -- `let <bindings> <body>`
-  | letE    : List (String × Expr) → Expr → Expr
+  | letE     : List (Name × Expr) → Expr → Expr
   -- `letrec <bindings> <body>`
-  | letRecE : List (String × Expr) → Expr → Expr
-  -- `<fun> <args>`
-  | app     : Expr → List Expr → Expr 
+  | letRecE  : List (Name × Expr) → Expr → Expr
+  -- `mutrec <bindings> <body>`
+  | mutRecE  : List (Name × Expr) → Expr → Expr
+  -- `<fun>`
+  | app₀     : Expr → Expr
+  -- `<fun> <arg>`
+  | app      : Expr → Expr → Expr
   -- `quote <datum>`
-  | quote   : SExpr → Expr
-  -- `<unaryop> <e>`
-  | unaryOp : UnaryOp → Expr → Expr
+  | quote    : SExpr → Expr
   -- `<binop> <e1> <e2>`
-  | binOp   : BinOp → Expr → Expr → Expr    
+  | binaryOp : BinaryOp → Expr → Expr → Expr
+  -- `<cons> <e1> <e2>`
+  | cons : Expr → Expr → Expr
+  -- `<strcons> <e1> <e2>`
+  | strcons  : Expr → Expr → Expr
+  -- `atom <e>`
+  | atom     : Expr → Expr
+  -- `car <e>`
+  | car      : Expr → Expr
+  -- `cdr <e>`
+  | cdr      : Expr → Expr
   -- `emit <e>`
-  | emit    : Expr → Expr
-  -- `begin <e1> <e2> ... `
-  | begin   : List Expr → Expr
+  | emit     : Expr → Expr
+  -- `begin <e1> <e2> ...`
+  | begin    : List Expr → Expr
   -- `current-env`
-  | currEnv : Expr
-  -- `eval <expr> <env>`
-  | eval    : Expr → Option Expr → Expr
-  deriving Repr
+  | currEnv  : Expr
+  deriving Repr, BEq, Inhabited
+
+namespace Expr
+
+def mkApp (f : Expr) : List Expr → Expr
+  | a :: as => as.foldl (init := .app f a) fun acc a => .app acc a
+  | [] => .app₀ f
+
+def mkListWith (es : List Expr) (tail : Expr) : Expr := 
+  es.foldr (init := tail)
+    fun n acc => .cons n acc
+
+def mkList (es : List Expr) : Expr :=   
+  mkListWith es (.lit .nil)
+
+/--
+Remove all binders from an expression, converting a lambda into
+an "implicit lambda". This is useful for constructing the `rhs` of
+recursor rules.
+-/
+def toImplicitLambda : Expr → Expr
+  | .lam _ body => toImplicitLambda body
+  | x => x
+
+class ToExpr (α : Type u) where 
+  toExpr : α → Expr 
+
+instance : ToExpr Nat where 
+  toExpr n := .lit $ .num (Fin.ofNat n)
+  
+instance : ToExpr Int where 
+  toExpr n := .lit $ .num (Fin.ofInt n)
+
+instance : ToExpr (Fin N) where 
+  toExpr n := .lit $ .num n
+
+instance : ToExpr Name where 
+  toExpr := .sym
+
+/-- Non-instance version when we want lurk-friendly names -/
+def toExprFix (n : Name) : Expr := 
+  .sym (fixName n false)
+
+instance : ToExpr String where 
+  toExpr s := .lit $ .str s
+
+instance : ToExpr Char where 
+  toExpr c := .lit $ .char c
+
+instance : ToExpr Expr where 
+  toExpr s := s
--- lakefile.lean
+++ lakefile.lean
@@ -1,9 +1,19 @@
 import Lake
 open Lake DSL
 
-package lurk 
+package Lurk
+
+lean_lib Lurk
 
 @[defaultTarget]
 lean_exe lurk {
   root := `Main
 }
+
+require YatimaStdLib from git
+  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "dd565ffec739f9ee0a79a3bf47ab5e1e0db0d8e2"
+
+require LSpec from git
+  "https://github.com/yatima-inc/LSpec.git" @ "a8dc2f38fc38f16efcc877ca8a4c7b37d3965db0"
+
+lean_exe Tests.Evaluation
