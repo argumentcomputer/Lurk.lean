66976b6d284be75fc607a067509d903549961ac5
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-10-10 17:57:55 -0300

    some progress

--- Lurk/Scalar.lean
+++ Lurk/Scalar.lean
@@ -5,20 +5,42 @@ namespace Lurk
 inductive Tag where
   | nil | cons | num
 
-def Tag.hash : Tag → Fin N
-  | .nil  => ⟨0, by simp⟩
-  | .cons => ⟨1, by simp⟩
-  | .num  => ⟨2, by simp⟩
+abbrev F := Fin N
+
+def Tag.hash : Tag → F
+  | .nil  => .ofNat 0
+  | .cons => .ofNat 1
+  | .num  => .ofNat 2
 
 structure ScalarPtr where
-  tag : Fin N
-  val : Fin N
+  tag : F
+  val : F
 
 inductive ScalarExpr where
-| nil
-| cons (car: ScalarPtr) (cdr: ScalarPtr)
-| num (val: Fin N)
+  | nil
+  | cons (car: ScalarPtr) (cdr: ScalarPtr)
+  | num (val: F)
+  | chr (x : F)
+  | str (head : Char) (tail : ScalarPtr)
 
-def ScalarExpr.hash : ScalarExpr → Fin N := sorry
+def hashChar (c : Char) : F :=
+  sorry
+
+def hashString : String → F
+  | ⟨[]⟩ => .ofNat 0
+  | ⟨c :: cs⟩ =>
+    let s := String.mk cs
+    sorry
+
+def hash3 : F → F → F → F := sorry
+
+def hash4 : F → F → F → F → F := sorry
+
+def ScalarExpr.hash : ScalarExpr → F
+  | .nil => hashString "nil"
+  | .cons car cdr => hash4 car.tag car.val cdr.tag cdr.val
+  | .num x => x
+  | .chr x => sorry
+  | .str c x => hash3 (hashChar c) x.tag x.val
 
 end Lurk
