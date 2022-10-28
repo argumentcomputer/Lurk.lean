dfc3d0c5d605ffd43cf981761adb9b2ef0c87d0f
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-10-28 18:50:10 -0300

    stactic ASTs

--- Lurk/Syntax2/DSL.lean
+++ Lurk/Syntax2/DSL.lean
@@ -13,20 +13,30 @@ scoped syntax "-"   : sym
 scoped syntax "/"   : sym
 scoped syntax "="   : sym
 scoped syntax "<"   : sym
-scoped syntax "<="  : sym
 scoped syntax ">"   : sym
+scoped syntax "<="  : sym
 scoped syntax ">="  : sym
 
-declare_syntax_cat           ast
-scoped syntax num          : ast
-scoped syntax char         : ast
-scoped syntax str          : ast
-scoped syntax sym          : ast
-scoped syntax "(" ast* ")" : ast
+declare_syntax_cat                     ast
+scoped syntax num                    : ast
+scoped syntax char                   : ast
+scoped syntax str                    : ast
+scoped syntax sym                    : ast
+scoped syntax "(" ast* ")"           : ast
+scoped syntax "(" ast+ " . " ast ")" : ast
+scoped syntax "," noWs ast           : ast -- escaping stactic ASTs
 
 def elabSym : TSyntax `sym → TermElabM Lean.Expr
   | `(sym| $i:ident) => mkAppM ``AST.sym #[mkStrLit i.getId.toString]
-  | `(sym| +) => mkAppM ``AST.sym #[mkStrLit "+"]
+  | `(sym| +)  => mkAppM ``AST.sym #[mkStrLit "+"]
+  | `(sym| *)  => mkAppM ``AST.sym #[mkStrLit "*"]
+  | `(sym| -)  => mkAppM ``AST.sym #[mkStrLit "-"]
+  | `(sym| /)  => mkAppM ``AST.sym #[mkStrLit "/"]
+  | `(sym| =)  => mkAppM ``AST.sym #[mkStrLit "="]
+  | `(sym| <)  => mkAppM ``AST.sym #[mkStrLit "<"]
+  | `(sym| >)  => mkAppM ``AST.sym #[mkStrLit ">"]
+  | `(sym| <=) => mkAppM ``AST.sym #[mkStrLit "<="]
+  | `(sym| >=) => mkAppM ``AST.sym #[mkStrLit ">="]
   | _ => throwUnsupportedSyntax
 
 partial def elabAST : TSyntax `ast → TermElabM Lean.Expr
@@ -38,11 +48,12 @@ partial def elabAST : TSyntax `ast → TermElabM Lean.Expr
   | `(ast| $s:sym) => elabSym s
   | `(ast| ($xs*)) => xs.foldrM (init := Lean.mkConst ``AST.nil) fun x acc => do
     mkAppM ``AST.cons #[← elabAST x, acc]
+  | `(ast| ($xs* . $x)) => do
+    mkAppM ``AST.cons ((← xs.mapM elabAST).push (← elabAST x))
+  | `(ast| ,$x:ast) => do mkAppM ``AST.stac #[← elabAST x]
   | _ => throwUnsupportedSyntax
 
 elab "⟦ " x:ast " ⟧" : term =>
   elabAST x
-
-#reduce ⟦(+ a b 1)⟧
 
 end Lurk.Syntax.DSL
