b986e3e93ed054a7fb8067aa207bae67c463f147
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-10-30 19:14:30 -0300

    printing AST

--- Lurk/Syntax2/AST.lean
+++ Lurk/Syntax2/AST.lean
@@ -18,4 +18,23 @@ def mkCons (xs : List AST) (init : AST) : AST :=
 def mkQuote (x : AST) : AST :=
   mkCons [.sym "QUOTE", x] .nil
 
+def unfoldCons (acc : Array AST := #[]) : AST → Array AST
+  | cons x nil => acc.push x
+  | cons x y => y.unfoldCons $ acc.push x
+  | x => acc.push x
+
+open Std Format in
+partial def toFormat : AST → Format
+  | nil => "NIL"
+  | num n => format n
+  | char c => s!"#\\{c}"
+  | str s => s!"\"{s}\""
+  | sym s => s
+  | cns => match ((cns.unfoldCons).map toFormat).data with
+    | [] => "()"
+    | x :: xs => paren $ xs.foldl (fun acc x => group $ acc ++ line ++ x) x
+
+instance : Std.ToFormat AST := ⟨toFormat⟩
+instance : ToString AST := ⟨toString ∘ toFormat⟩
+
 end AST
