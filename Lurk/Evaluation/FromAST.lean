56f16cb78f2f16b784b21b56621ce95c8b45e817
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-11-04 12:01:14 -0300

    more cleanup

--- Lurk/Evaluation/FromAST.lean
+++ Lurk/Evaluation/FromAST.lean
@@ -6,20 +6,15 @@ open Evaluation (Expr)
 
 abbrev ToExprM := Except String
 
-def mkArgs : AST → ToExprM (List String)
+def asArgs : AST → ToExprM (List String)
   | .nil => return []
-  | .cons (.sym x) xs => return x :: (← mkArgs xs)
+  | .cons (.sym x) xs => return x :: (← xs.asArgs)
   | x => throw s!"expected list of symbols but got {x}"
 
-def mkBindings : AST → ToExprM (List (String × AST))
+def asBindings : AST → ToExprM (List (String × AST))
   | .nil => return []
-  | .cons ~[.sym x, y] xs => return (x, y) :: (← mkBindings xs)
+  | .cons ~[.sym x, y] xs => return (x, y) :: (← xs.asBindings)
   | x => throw s!"expected list of (symbol, body) pairs but got {x}"
-
-def toList : AST → ToExprM (List AST)
-  | .nil => return []
-  | .cons x xs => return x :: (← toList xs)
-  | x => throw s!"expected list but got {x}"
 
 def mkOp₁ (op₁ : String) : Expr → Expr := match op₁ with
   | "ATOM"   => .op₁ .atom
@@ -70,19 +65,19 @@ partial def toExpr : AST → ToExprM Expr
   | ~[.sym "IF", x, y, z] => return .if (← x.toExpr) (← y.toExpr) (← z.toExpr)
   -- `lambda` requires a gradual consumption of a symbol
   | ~[.sym "LAMBDA", args, body] => do
-    let args ← mkArgs args
+    let args ← args.asArgs
     if args.isEmpty then
       return .lambda "_" (← body.toExpr)
     else
       return args.foldr (init := ← body.toExpr) fun arg acc => .lambda arg acc
   -- let and letrec are in the same case
   | ~[.sym "LET", bindings, body] => do
-    let bindings ← mkBindings bindings
+    let bindings ← bindings.asBindings
     let bindings ← bindings.mapM fun (x, y) => return (x, ← y.toExpr)
     return bindings.foldr (init := ← body.toExpr)
       fun (n, e) acc => .let n e acc
   | ~[.sym "LETREC", bindings, body] => do
-    let bindings ← mkBindings bindings
+    let bindings ← bindings.asBindings
     let bindings ← bindings.mapM fun (x, y) => return (x, ← y.toExpr)
     return bindings.foldr (init := ← body.toExpr)
       fun (n, e) acc => .letrec n e acc
@@ -92,11 +87,12 @@ partial def toExpr : AST → ToExprM Expr
   -- everything else is just an `app`
   -- unary operators
   | ~[.sym op₁, x] => return mkOp₁ op₁ (← x.toExpr)
-  | cons fn args => do
-    let args ← (← toList args) |>.mapM toExpr
-    if args.isEmpty then
-      return .app₀ (← fn.toExpr)
-    else
-      return args.foldl (init := ← fn.toExpr) fun acc arg => .app acc arg
+  | cons fn args => match args.telescopeCons with
+    | (args, nil) =>
+      return if args.isEmpty then
+        .app₀ (← fn.toExpr)
+      else
+        (← args.mapM toExpr).foldl (fun acc arg => .app acc arg) (← fn.toExpr)
+    | x => throw s!"expected a list terminating with `nil` but got {x}"
 
 end Lurk.Syntax.AST
