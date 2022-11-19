1ca9b70d0fe982773fc499e7cdc76801a6ef60c2
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-11-19 09:26:53 -0300

    remove escaping from Lean.Name on ToAST instance (fix)

--- Lurk/Syntax/AST.lean
+++ Lurk/Syntax/AST.lean
@@ -105,7 +105,7 @@ instance : ToAST String where
 
 /-- This instance is dangerously lossy -/
 instance (priority := low) : ToAST Lean.Name where
-  toAST n := .sym n.toString
+  toAST n := .sym $ n.toString false
 
 instance [ToAST α] : ToAST (List α) where
   toAST es := AST.consWith (es.map toAST) .nil
