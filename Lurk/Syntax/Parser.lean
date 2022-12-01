ad36e6065131b4257b76006044216f70f159ce74
Ilona Prikule <ilona@yatima.io>
2022-12-01 19:26:20 +0100

    Change parser to use a bit more readable combinators

--- Lurk/Syntax/Parser.lean
+++ Lurk/Syntax/Parser.lean
@@ -1,9 +1,10 @@
+import Megaparsec.Char
 import Megaparsec.Common
 import Lurk.Syntax.AST
 
 namespace Lurk.Syntax
 
-open Megaparsec Parsec Common Lurk.Syntax AST
+open Megaparsec Char Parsec Common Lurk.Syntax AST
 
 abbrev P := Parsec Char String Unit
 
@@ -18,17 +19,12 @@ def numP : P AST := do
 --   discard $ single '\''
 --   return .char c
 
-def charP : P AST := attempt do
-  discard $ single '#'
-  discard $ single '\\'
-  let c ← satisfy fun _ => true
-  return .char c
+def charP : P AST := do
+  discard $ string "#\\"
+  .char <$> anySingle
 
-def strP : P AST := do
-  discard $ single '"'
-  let x ← many' (satisfy fun c => c != '\"')
-  discard $ single '"'
-  return .str $ ⟨x⟩
+def strP : P AST := between '"' '"' $
+  .str <$> String.mk <$> many' (satisfy fun c => c != '\"')
 
 def validSpecialSymChar : Char → Bool
   | '+'
@@ -50,17 +46,14 @@ def noEscSymP : P AST := do
   else
     return .sym i
 
-def escSymP : P AST := do
-  discard $ single '|'
-  let x ← many' (satisfy fun c => c != '|')
-  discard $ single '|'
-  return .sym $ ⟨x⟩
+def escSymP : P AST := between '|' '|' $
+  .sym <$> String.mk <$> many' (satisfy fun c => c != '|')
 
 def symP : P AST :=
   escSymP <|> noEscSymP
 
-def blanksP : P Unit := do
-  discard $ many' $ satisfy [' ', '\n', '\t'].contains
+def blanksP : P Unit :=
+  discard $ many' $ oneOf [' ', '\n', '\t']
 
 def atomP : P AST :=
   symP <|> numP <|> charP <|> strP
@@ -72,19 +65,15 @@ partial def quoteP : P AST := do
   let x ← astP
   return mkQuote x
 
-partial def listP : P AST := do
-  discard $ single '('
+partial def listP : P AST := between '(' ')' $ do
   blanksP
   let x ← many' astP
-  discard $ single ')'
   return consWith x nil
 
-partial def dottedListP : P AST := do
-  discard $ single '('
-  let xs ← many1' astP
+partial def dottedListP : P AST := between '(' ')' $ do
+  let xs ← some' astP
   discard $ single '.'
   let x ← astP
-  discard $ single ')'
   return consWith xs x
 
 partial def astP : P AST := do
@@ -96,8 +85,6 @@ partial def astP : P AST := do
 end
 
 protected def parse (code : String) : Except String AST :=
-  match parse astP code with
-  | .ok x => return x
-  | .error x => throw $ toString x
+  Except.mapError toString $ parse astP code
 
 end Lurk.Syntax
