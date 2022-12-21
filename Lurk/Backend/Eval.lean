94c5a552c41b6a957e2129963aa0996581076c04
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-12-21 10:23:04 -0300

    Adding frames for better debugging in case of evaluation errors (#37)

--- Lurk/Backend/Eval.lean
+++ Lurk/Backend/Eval.lean
@@ -1,12 +1,18 @@
 import Lurk.Backend.Expr
+import Lurk.Backend.ExprUtils
+import Std.Data.RBMap
 
 namespace Lurk.Backend
 
 set_option genSizeOfSpec false in
 mutual
 
+inductive Frames
+  | mk : List (Expr × Env) → Frames
+
 inductive Env
-  | mk : Lean.RBNode String (fun _ => Thunk (Except String Value)) → Env
+  | mk : Lean.RBNode String
+    (fun _ => Thunk (Except (String × Frames) Value)) → Env
 
 inductive Value where
   | num  : Fin N  → Value
@@ -97,7 +103,7 @@ instance : ToString Value where
 
 instance : Inhabited Env := ⟨.mk .leaf⟩
 
-abbrev Result := Except String Value
+abbrev Result := Except (String × Frames) Value
 
 namespace Env
 
@@ -110,7 +116,30 @@ def insert (s : String) (v : Thunk Result) : Env → Env
 def toArray : Env → Array (String × (Thunk Result))
   | mk e => e.fold (init := #[]) fun acc k v => acc.push (k, v)
 
+def toRBMap : Env → Std.RBMap String (Thunk Result) compare
+  | mk e => e.fold (init := default) fun acc k v => acc.insert k v
+
 end Env
+
+instance : ToString (String × Frames) := ⟨Prod.fst⟩
+
+def Frames.pprint (n : Nat) : Frames → String
+  | .mk frames =>
+    let rec aux (acc : String) : List (Expr × Env) → String
+      | [] => acc
+      | f :: fs => aux (frameToString f) fs
+    aux default (frames.take n)
+where
+  frameToString : Expr × Env → String
+    | (e, env) =>
+      let fVars := e.getFreeVars
+      -- considering relevant variables, only
+      let env := env.toRBMap |>.filter fun s _ => fVars.contains s
+      let init := "############################################################"
+        ++ e.toString
+        ++ "------------------------------------------------------------"
+      env.foldl (init := init) fun acc s v =>
+        match v.get with | .ok x | .error x => s!"{acc}\n{s} → {x}"
 
 mutual
 
@@ -118,8 +147,9 @@ partial def Env.eq (e₁ e₂ : Env) : Bool :=
   let rec aux : List (String × Result) → List (String × Result) → Bool
     | [], [] => true
     | (s₁, v₁)::xs, (s₂, v₂)::ys => match (v₁, v₂) with
-      | (Except.ok    v₁, Except.ok    v₂) => v₁.beq v₂ && s₁ == s₂ && aux xs ys
-      | (Except.error e₁, Except.error e₂) => e₁ == e₂  && s₁ == s₂ && aux xs ys
+      | (Except.ok v₁, Except.ok v₂) => v₁.beq v₂ && s₁ == s₂ && aux xs ys
+      | (Except.error (e₁, _), Except.error (e₂, _)) =>
+        e₁ == e₂  && s₁ == s₂ && aux xs ys
       | _ => false
     | _, _ => false
   aux
@@ -152,147 +182,150 @@ instance : Coe String Value := ⟨.str⟩
 instance : OfNat Value n where
   ofNat := .num (.ofNat n)
 
-def error (e : Expr) (msg : String) : Result :=
-  throw s!"Error when evaluating {e}:\n{msg}"
+def error (e : Expr) (frames : Frames) (msg : String) : Result :=
+  throw (s!"Error when evaluating {e}:\n{msg}", frames)
 
-def numAdd (e : Expr) : Value → Value → Result
+def numAdd (e : Expr) (frames : Frames) : Value → Value → Result
   | .num x, .num y => return .num (x + y)
   | .u64 x, .u64 y => return .u64 (x + y)
   | .num x, .u64 y => return .num (x + (.ofNat y.toNat))
   | .u64 x, .num y => return .num ((.ofNat x.toNat) + y)
-  | v₁, v₂ => error e s!"expected numeric values, got\n  {v₁} and {v₂}"
+  | v₁, v₂ => error e frames s!"expected numeric values, got\n  {v₁} and {v₂}"
 
-def numSub (e : Expr) : Value → Value → Result
+def numSub (e : Expr) (frames : Frames) : Value → Value → Result
   | .num x, .num y => return .num (x - y)
   | .u64 x, .u64 y => return .u64 (x - y)
   | .num x, .u64 y => return .num (x - (.ofNat y.toNat))
   | .u64 x, .num y => return .num ((.ofNat x.toNat) - y)
-  | v₁, v₂ => error e s!"expected numeric values, got\n  {v₁} and {v₂}"
+  | v₁, v₂ => error e frames s!"expected numeric values, got\n  {v₁} and {v₂}"
 
-def numMul (e : Expr) : Value → Value → Result
+def numMul (e : Expr) (frames : Frames) : Value → Value → Result
   | .num x, .num y => return .num (x * y)
   | .u64 x, .u64 y => return .u64 (x * y)
   | .num x, .u64 y => return .num (x * (.ofNat y.toNat))
   | .u64 x, .num y => return .num ((.ofNat x.toNat) * y)
-  | v₁, v₂ => error e s!"expected numeric values, got\n  {v₁} and {v₂}"
+  | v₁, v₂ => error e frames s!"expected numeric values, got\n  {v₁} and {v₂}"
 
-def numDiv (e : Expr) : Value → Value → Result
+def numDiv (e : Expr) (frames : Frames) : Value → Value → Result
   | .num x, .num y => return .num (x / y)
   | .u64 x, .u64 y => return .u64 (x / y)
   | .num x, .u64 y => return .num (x / (.ofNat y.toNat))
   | .u64 x, .num y => return .num ((.ofNat x.toNat) / y)
-  | v₁, v₂ => error e s!"expected numeric values, got\n  {v₁} and {v₂}"
+  | v₁, v₂ => error e frames s!"expected numeric values, got\n  {v₁} and {v₂}"
 
-def numEq (e : Expr) : Value → Value → Result
+def numEq (e : Expr) (frames : Frames) : Value → Value → Result
   | .num x, .num y => return decide (x == y)
   | .u64 x, .u64 y => return decide (x == y)
   | .num x, .u64 y => return decide (x == (.ofNat y.toNat))
   | .u64 x, .num y => return decide ((.ofNat x.toNat) == y)
-  | v₁, v₂ => error e s!"expected numeric values, got\n  {v₁} and {v₂}"
+  | v₁, v₂ => error e frames s!"expected numeric values, got\n  {v₁} and {v₂}"
 
-def numLt (e : Expr) : Value → Value → Result
+def numLt (e : Expr) (frames : Frames) : Value → Value → Result
   | .num x, .num y => return F.lt x y
   | .u64 x, .u64 y => return decide (x < y)
   | .num x, .u64 y => return F.lt x (.ofNat y.toNat)
   | .u64 x, .num y => return F.lt (.ofNat x.toNat) y
-  | v₁, v₂ => error e s!"expected numeric values, got\n  {v₁} and {v₂}"
+  | v₁, v₂ => error e frames s!"expected numeric values, got\n  {v₁} and {v₂}"
 
-def numGt (e : Expr) : Value → Value → Result
+def numGt (e : Expr) (frames : Frames) : Value → Value → Result
   | .num x, .num y => return F.gt x y
   | .u64 x, .u64 y => return decide (x > y)
   | .num x, .u64 y => return F.gt x (.ofNat y.toNat)
   | .u64 x, .num y => return F.gt (.ofNat x.toNat) y
-  | v₁, v₂ => error e s!"expected numeric values, got\n  {v₁} and {v₂}"
+  | v₁, v₂ => error e frames s!"expected numeric values, got\n  {v₁} and {v₂}"
 
-def numLe (e : Expr) : Value → Value → Result
+def numLe (e : Expr) (frames : Frames) : Value → Value → Result
   | .num x, .num y => return F.le x y
   | .u64 x, .u64 y => return decide (x <= y)
   | .num x, .u64 y => return F.le x (.ofNat y.toNat)
   | .u64 x, .num y => return F.le (.ofNat x.toNat) y
-  | v₁, v₂ => error e s!"expected numeric values, got\n  {v₁} and {v₂}"
+  | v₁, v₂ => error e frames s!"expected numeric values, got\n  {v₁} and {v₂}"
 
-def numGe (e : Expr) : Value → Value → Result
+def numGe (e : Expr) (frames : Frames) : Value → Value → Result
   | .num x, .num y => return F.ge x y
   | .u64 x, .u64 y => return decide (x >= y)
   | .num x, .u64 y => return F.ge x (.ofNat y.toNat)
   | .u64 x, .num y => return F.ge (.ofNat x.toNat) y
-  | v₁, v₂ => error e s!"expected numeric values, got\n  {v₁} and {v₂}"
+  | v₁, v₂ => error e frames s!"expected numeric values, got\n  {v₁} and {v₂}"
 
-def Expr.evalOp₁ (e : Expr) : Op₁ → Value → Result
+def Expr.evalOp₁ (e : Expr) (frames : Frames) : Op₁ → Value → Result
   | .atom, .cons .. => return .nil
   | .atom, _ => return .t
   | .car, .cons car _ => return car
   | .car, .str ⟨[]⟩ => return .nil
   | .car, .str ⟨h::_⟩ => return .char h
-  | .car, v => error e s!"expected cons value, got\n  {v}"
+  | .car, v => error e frames s!"expected cons value, got\n  {v}"
   | .cdr, .cons _ cdr => return cdr
   | .cdr, .str ⟨[]⟩ => return .str ""
   | .cdr, .str ⟨_::t⟩ => return .str ⟨t⟩
-  | .cdr, v => error e s!"expected cons value, got\n  {v}"
+  | .cdr, v => error e frames s!"expected cons value, got\n  {v}"
   | .emit, v => dbg_trace v; return v
   | .commit, v => return .u64 v.toString.hash
   | .comm, .num n => return .comm n
-  | .comm, v => error e s!"expected a num, got\n  {v}"
-  | .open, _ => error e "TODO open"
+  | .comm, v => error e frames s!"expected a num, got\n  {v}"
+  | .open, _ => error e frames "TODO open"
   | .num, x@(.num _) => return x
   | .num, .u64 n => return .num (.ofNat n.toNat)
   | .num, .char c => return .num (.ofNat c.toNat)
   | .num, .comm c => return .num c
-  | .num, v => error e s!"expected char, num, u64, or comm value, got\n  {v}"
+  | .num, v => error e frames s!"expected char, num, u64, or comm value, got\n  {v}"
   | .u64, x@(.u64 _) => return x
   | .u64, .num n => return .u64 (.ofNat n.val)
-  | .u64, v => error e s!"expected num or u64, got\n  {v}"
+  | .u64, v => error e frames s!"expected num or u64, got\n  {v}"
   | .char, .char c => return .char c
   | .char, .num ⟨n, _⟩ =>
     let charVal := n.toUInt32
     if h : isValidChar charVal then return .char ⟨charVal, h⟩
-    else error e s!"{charVal} is not a valid char value"
-  | .char, v => error e s!"expected char or num value, got\n  {v}"
+    else error e frames s!"{charVal} is not a valid char value"
+  | .char, v => error e frames s!"expected char or num value, got\n  {v}"
 
-def Expr.evalOp₂ (e : Expr) : Op₂ → Value → Value → Result
+def Expr.evalOp₂ (e : Expr) (frames : Frames) : Op₂ → Value → Value → Result
   | .cons, v₁, v₂ => return .cons v₁ v₂
   | .strcons, .char c, .str s => return .str ⟨c :: s.data⟩
-  | .strcons, v₁, v₂ => error e s!"expected char and string value, got {v₁} and {v₂}"
-  | .add, v₁, v₂ => numAdd e v₁ v₂
-  | .sub, v₁, v₂ => numSub e v₁ v₂
-  | .mul, v₁, v₂ => numMul e v₁ v₂
-  | .div, v₁, v₂ => numDiv e v₁ v₂
-  | .numEq, v₁, v₂ => numEq e v₁ v₂
-  | .lt, v₁, v₂ => numLt e v₁ v₂
-  | .gt, v₁, v₂ => numGt e v₁ v₂
-  | .le, v₁, v₂ => numLe e v₁ v₂
-  | .ge, v₁, v₂ => numGe e v₁ v₂
+  | .strcons, v₁, v₂ => error e frames s!"expected char and string value, got {v₁} and {v₂}"
+  | .add, v₁, v₂ => numAdd e frames v₁ v₂
+  | .sub, v₁, v₂ => numSub e frames v₁ v₂
+  | .mul, v₁, v₂ => numMul e frames v₁ v₂
+  | .div, v₁, v₂ => numDiv e frames v₁ v₂
+  | .numEq, v₁, v₂ => numEq e frames v₁ v₂
+  | .lt, v₁, v₂ => numLt e frames v₁ v₂
+  | .gt, v₁, v₂ => numGt e frames v₁ v₂
+  | .le, v₁, v₂ => numLe e frames v₁ v₂
+  | .ge, v₁, v₂ => numGe e frames v₁ v₂
   | .eq, v₁, v₂ => return v₁.beq v₂
-  | .hide, _, _ => error e "TODO hide"
+  | .hide, _, _ => error e frames "TODO hide"
 
-partial def Expr.eval (env : Env := default) : Expr → Result
+partial def Expr.eval
+    (e : Expr) (env : Env := default) (frames : Frames := .mk []) : Result :=
+  let frames := match frames with | .mk frames => .mk $ (e, env) :: frames
+  match e with
   | .atom a => return .ofAtom a
   | .sym n => match env.find? n with
     | some v => v.get
-    | none => throw s!"{n} not found"
+    | none => error e frames s!"{n} not found"
   | .env =>
     env.toArray.foldrM (init := .nil)
       fun (k, v) acc => return .cons (.cons (.sym k) (← v.get)) acc
-  | .begin x (atom .nil) => x.eval env
-  | .begin x y => do discard $ x.eval env; y.eval env
-  | .if x y z => do match ← x.eval env with
-    | .nil => z.eval env
-    | _ => y.eval env
-  | .app₀ fn => do match ← fn.eval env with
-    | .fun "_" env' body => body.eval env'
-    | _ => throw s!"error evaluating\n{fn}\ninvalid 0-arity app"
-  | .app fn arg => do match ← fn.eval env with
-    | .fun "_" .. => throw s!"error evaluating\n{fn}\ncannot apply argument to 0-arg lambda"
-    | .fun n env' body => body.eval (env'.insert n (arg.eval env))
-    | x => throw s!"error evaluating\n{fn}\nlambda was expected, got\n  {x}"
-  | x@(.op₁ op e) => do evalOp₁ x op (← e.eval env)
-  | x@(.op₂ op e₁ e₂) => do evalOp₂ x op (← e₁.eval env) (← e₂.eval env)
+  | .begin x (atom .nil) => x.eval env frames
+  | .begin x y => do discard $ x.eval env frames; y.eval env frames
+  | .if x y z => do match ← x.eval env frames with
+    | .nil => z.eval env frames
+    | _ => y.eval env frames
+  | .app₀ fn => do match ← fn.eval env frames with
+    | .fun "_" env' body => body.eval env' frames
+    | _ => error e frames s!"error evaluating\n{fn}\ninvalid 0-arity app"
+  | .app fn arg => do match ← fn.eval env frames with
+    | .fun "_" .. => error e frames s!"error evaluating\n{fn}\ncannot apply argument to 0-arg lambda"
+    | .fun n env' body => body.eval (env'.insert n (arg.eval env frames)) frames
+    | x => error e frames s!"error evaluating\n{fn}\nlambda was expected, got\n  {x}"
+  | x@(.op₁ op e) => do evalOp₁ x frames op (← e.eval env frames)
+  | x@(.op₂ op e₁ e₂) => do evalOp₂ x frames op (← e₁.eval env frames) (← e₂.eval env frames)
   | .lambda s e => return .fun s env e
-  | .let s v b => b.eval (env.insert s (v.eval env))
+  | .let s v b => b.eval (env.insert s (v.eval env frames)) frames
   | .letrec s v b => do
     let v' : Expr := .letrec s v v
-    let env' := env.insert s $ .mk fun _ => eval env v'
-    b.eval (env.insert s (eval env' v))
+    let env' := env.insert s $ .mk fun _ => v'.eval env frames
+    b.eval (env.insert s (v.eval env' frames)) frames
   | .quote d => return .ofDatum d
 
 end Lurk.Backend
