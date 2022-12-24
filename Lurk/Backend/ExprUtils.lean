b3199bcea982a94797586a6f6613e36cf28fcfc5
Hanting Zhang <hantingz@usc.edu>
2022-12-23 20:26:48 -0800

    use `find?`

--- Lurk/Backend/ExprUtils.lean
+++ Lurk/Backend/ExprUtils.lean
@@ -178,9 +178,9 @@ def mutualize (binders : List $ String × Expr) : List $ String × Expr :=
   let names := binders.map Prod.fst
   let binders := Std.RBMap.ofList binders compare
   let blocks := Lean.SCC.scc names fun name =>
-    binders.find! name |>.getReferences.toList
+    binders.find? name |>.get! |>.getReferences.toList
   List.join <| blocks.map fun block =>
-    let block := block.map fun name => (name, binders.find! name)
+    let block := block.map fun name => (name, binders.find? name |>.get!)
     mkMutualBlock block
 
 namespace Anon
