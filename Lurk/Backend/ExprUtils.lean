0cbbdac593f3c906275b03b3f5f47e63a44f0874
DanielRrr <relationalgebra@gmail.com>
2023-02-08 22:42:42 +0000

    introduce inlining binders that used only once

--- Lurk/Backend/ExprUtils.lean
+++ Lurk/Backend/ExprUtils.lean
@@ -109,6 +109,31 @@ def replaceFreeVars (map : Std.RBMap String Expr compare) : Expr → Expr
     .if (e₁.replaceFreeVars map) (e₂.replaceFreeVars map) (e₃.replaceFreeVars map)
   | x => x
 
+def inlineBinder (expr : Expr) : Expr :=
+  match expr with
+    | x@(.letrec s v b)
+    | x@(.let s v b) =>
+        let letrec := x matches .letrec _ _ _
+        let (bs, b) := if letrec then b.telescopeLetrec #[(s, v)] else b.telescopeLet #[(s, v)]
+        let numVarsBinder := Std.RBMap.filter (countFreeVarOccs default default v) fun _ x => x == 1
+        let numVarsBody := Std.RBMap.filter (countFreeVarOccs default default b) fun _ x => x == 1
+        let (bs, _) :=
+          bs.foldr
+            (fun (n, e) (accBinders, accFVars) =>
+              if numVarsBinder.find? n != some 1 && numVarsBody.find? n != some 1
+              then ((n, e) :: accBinders, (accFVars.erase fun s' => compare s' s).union -- `s` is no longer a free variable TODO double-check ordering of arguments to "compare"
+                     $ v.getFreeVars (if letrec then .single n else default))
+              else (accBinders, accFVars)
+            )
+            (init := (default, b.getFreeVars))
+        if letrec then mkLetrec bs (b.pruneBlocks default)
+        else mkLet bs (b.pruneBlocks default)
+    | a => a
+
+#eval pruneBlocks default (.let "a" (.atom .nil) (.sym "a"))
+#eval inlineBinder (.let "a" (.atom .nil) (.sym "a"))
+
+
 def mkIfElses (ifThens : List (Expr × Expr)) (finalElse : Expr := .atom .nil) : Expr :=
   match ifThens with
   | [] => .atom .nil
